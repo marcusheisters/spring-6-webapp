26eff5732f7de8f5e9af215ac20c65e8368ea45c
Marcus Heisters <marcus.heisters@googlemail.com>
2023-09-30 16:14:40 +0200

    Add relationships for author and books

--- src/main/java/guru/springframework/spring6webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -1,20 +1,23 @@
 package guru.springframework.spring6webapp.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 /**
  * @author Marcus Heisters
  */
 @Entity
 public class Author {
+
     @Id
     @GeneratedValue (strategy = GenerationType.AUTO)
     private long id;
+
     private String firstName;
     private String lastName;
+    @ManyToMany (mappedBy = "authors")
+    private Set<Book> books;
 
     public void setId(Long id) {
         this.id = id;
@@ -26,5 +29,28 @@ public class Author {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 }
--- src/main/java/guru/springframework/spring6webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -1,10 +1,9 @@
 package guru.springframework.spring6webapp.domain;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 /**
  * @author Marcus Heisters
@@ -13,10 +12,33 @@ import jakarta.persistence.Id;
 public class Book {
 
     @Id
-    @GeneratedValue (strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
+
     private String isbn;
+
+    @ManyToMany(mappedBy = "books")
+    @JoinTable(name = "author_book",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 
     public void setId(Long id) {
         this.id = id;
