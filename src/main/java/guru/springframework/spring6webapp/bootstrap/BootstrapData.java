a9aeae0fa26c29e0b5784dac2dcd34bf67d45db6
Marcus Heisters <marcus.heisters@googlemail.com>
2023-10-01 17:23:54 +0200

    Test Commit

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -39,10 +39,9 @@ public class BootstrapData implements CommandLineRunner {
         rod.setLastName("Johnson");
 
         Book noEJB = new Book();
-        noEJB.setName
-                ("J2EE Development without EJB");
+        noEJB.setName("J2EE Development without EJB");
         noEJB.setIsbn("54757585");
-
+        // Test
         Author rodSaved = authorRepository.save(rod);
         Book noEJBSaved = bookRepository.save(noEJB);
 
