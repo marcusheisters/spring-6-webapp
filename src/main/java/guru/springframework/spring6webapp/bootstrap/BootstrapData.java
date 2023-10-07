ada4fe95bd832e181affc4ab90104eeda23b7331
Marcus Heisters <marcus.heisters@googlemail.com>
2023-10-07 13:04:30 +0200

    - Refactoring and comments

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -10,7 +10,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 /**
- * Created by jt, Spring Framework Guru.
+ * Generates bootstrap data for the h2 database. CommandLineRunner indicates that the run method
+ * will be run when its contained within a spring application.
  */
 @Component
 public class BootstrapData implements CommandLineRunner {
@@ -28,30 +29,37 @@ public class BootstrapData implements CommandLineRunner {
         this.publisherRepository = publisherRepository;
     }
 
+    /**
+     * Create bootstrap data when starting the application.
+     *
+     * @param args
+     * @throws Exception
+     */
     @Override
     public void run(String... args) throws Exception {
+        // Generate and persist two authors
         Author eric = new Author();
         eric.setFirstName("Eric");
         eric.setLastName("Evans");
-
-        Book ddd = new Book();
-        ddd.setTitle("Domain Driven Design");
-        ddd.setIsbn("123456");
-
         Author ericSaved = authorRepository.save(eric);
-        Book dddSaved = bookRepository.save(ddd);
 
         Author rod = new Author();
         rod.setFirstName("Rod");
         rod.setLastName("Johnson");
+        Author rodSaved = authorRepository.save(rod);
+
+        // Generate and persist two books
+        Book ddd = new Book();
+        ddd.setTitle("Domain Driven Design");
+        ddd.setIsbn("123456");
+        Book dddSaved = bookRepository.save(ddd);
 
         Book noEJB = new Book();
         noEJB.setTitle("J2EE Development without EJB");
         noEJB.setIsbn("54757585");
-        // Test
-        Author rodSaved = authorRepository.save(rod);
         Book noEJBSaved = bookRepository.save(noEJB);
 
+        // Add books to authors and vice versa
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
         dddSaved.getAuthors().add(ericSaved);
@@ -59,23 +67,23 @@ public class BootstrapData implements CommandLineRunner {
         authorRepository.save(ericSaved);
         authorRepository.save(rodSaved);
 
-
+        // Generate and persist publishers
         Publisher godalming = new Publisher();
         godalming.setPublisherName("Godalming");
         godalming.setCity("London");
         godalming.setAddress("Picadilly 2");
         godalming.setZip("33236");
-
         Publisher godalmingSaved = publisherRepository.save(godalming);
 
+        // Add publisher to books
         dddSaved.setPublisher(godalmingSaved);
         noEJBSaved.setPublisher(godalmingSaved);
 
-        authorRepository.save(ericSaved);
-        authorRepository.save(rodSaved);
+        // Save the books
         bookRepository.save(dddSaved);
         bookRepository.save(noEJBSaved);
 
+        // Reporting
         System.out.println("In Bootstrap");
         System.out.println("Author Count: " + authorRepository.count());
         System.out.println("Book Count: " + bookRepository.count());
